fc58f59c437a37c49c2732249fa79ef5eb029a86
Jason Son <jasonson2004@gmail.com>
2025-10-26 04:32:05 -0700

    changed cmd

--- src/commands/server.ts
+++ src/commands/server.ts
@@ -2,7 +2,7 @@ import { copyFile } from "fs/promises";
 import { join } from "path";
 
 async function init_server() {
-  await copyFile(join(__dirname, "../../../src/templates/server/logger.ts"), "src/logger.ts");
+  await copyFile(join(__dirname, "../../../axiomarc/src/templates/server/logger.ts"), "src/logger.ts");
 
   // add to package.json
   // create env config
